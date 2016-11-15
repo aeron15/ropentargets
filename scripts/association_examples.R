3d494e6529603ec40e36158de1dd9f23f43aab00
ckong <ckong@ebi.ac.uk>
2016-11-15 13:02:09 +0000

    By default method package does not load at startup

--- scripts/association_examples.R
+++ scripts/association_examples.R
@@ -1,3 +1,4 @@
+library(methods)
 # Interested in Inflammatory Bowel Disease and all the associations for this disease currently held in CTTV.
 # Firstly, we need to find the EFO identifier for "Inflammatory Bowel Disease".
 ensemblGeneID <- 'ENSG00000073756'
